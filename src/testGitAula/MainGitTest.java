c51822bd9561b95245156bcfc678c80fedc73050
Monique Basilio <monique.s.basilio@gmail.com>
2019-01-16 16:33:46 -0200

    Acrescentando Msg

--- src/testGitAula/MainGitTest.java
+++ src/testGitAula/MainGitTest.java
@@ -4,7 +4,7 @@ public class MainGitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Olá eu sou um teste sobre versionamento");
 	}
 
 }
